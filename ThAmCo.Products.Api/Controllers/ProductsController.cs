f752d48c87409849e20784d61242570e8b049c7d
T7037453 <T7037453@live.tees.ac.uk>
2022-11-05 02:16:43 +0000

    Scaffolded Controller

--- ThAmCo.Products.Api/Controllers/ProductsController.cs
+++ ThAmCo.Products.Api/Controllers/ProductsController.cs
@@ -80,8 +80,8 @@ namespace ThAmCo.Products.Api.Controllers
             _context.Products.Add(product);
             await _context.SaveChangesAsync();
 
-            //return CreatedAtAction("GetProduct", new { id = product.Id }, product);
-            return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
+            return CreatedAtAction("GetProduct", new { id = product.Id }, product);
+            //return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
         }
 
         // DELETE: api/Products/5
